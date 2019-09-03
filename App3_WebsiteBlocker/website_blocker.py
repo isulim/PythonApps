8ba651a2d983903c0d2663d49baee144c774de44
isulim <ihor.sulym@gmail.com>
2019-09-03 16:05:09 +0200

    Changing hosts file

--- App3_WebsiteBlocker/website_blocker.py
+++ App3_WebsiteBlocker/website_blocker.py
@@ -2,7 +2,7 @@ from platform import system
 from time import sleep
 from datetime import datetime
 
-hosts_path = ""
+hosts_path = "hosts"
 
 if system() == 'Windows':
     hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
@@ -13,8 +13,20 @@ redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com"]
 
 while True:
-    if 8 <= datetime.now().hour <= 16:
+    if 8 <= datetime.now().hour <= 15:
         print("Working hours...")
+        with open(hosts_path, 'r+') as hosts:
+            content = hosts.read()
+            for website in website_list:
+                if website not in content:
+                    hosts.write(redirect + "\t" + website + "\n")
     else:
         print("Relax time.")
-    sleep(10)
+        with open(hosts_path, 'r+') as hosts:
+            lines = hosts.readlines()
+            hosts.seek(0) # move cursor
+            for line in lines:
+                if not any (website in line for website in website_list):
+                    hosts.write(line)
+            hosts.truncate() # delete all after cursor
+    sleep(60)
