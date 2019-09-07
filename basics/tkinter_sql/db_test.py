e531339de39864235ac0db6f2126eea8099b978c
Igor Sulim <ihor.sulym@gmail.com>
2019-09-07 18:56:16 +0200

    Delete, Update functions

--- basics/tkinter_sql/db_test.py
+++ basics/tkinter_sql/db_test.py
@@ -16,7 +16,26 @@ def insert_data(item, quantity, price):
     conn.commit()
     conn.close()
 
+def delete_data(item):
+    conn = sqlite3.connect('lite3.db')
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM store WHERE item=?", (item,))
+    conn.commit()
+    conn.close()
+
+
+def update_data(item, quantity, price):
+    conn = sqlite3.connect('lite3.db')
+    cursor = conn.cursor()
+    cursor.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+
+
 if __name__ == "__main__":
-    create_table()
-    insert_data('Water Glass', 10, 4)
-    insert_data('Cofee Cup', '3', '7')
+    # create_table()
+    # insert_data('Wine Glass', 5, 7)
+    # insert_data('Water Glass', 10, 4)
+    # insert_data('Cofee Cup', '3', '7')
+    update_data('Wine Glass', 7, 10)
