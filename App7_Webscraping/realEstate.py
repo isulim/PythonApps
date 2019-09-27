a15c3ea8969be8bdbba75d45aec688a4a315156e
isulim <ihor.sulym@gmail.com>
2019-09-27 14:19:02 +0200

    Extracting data

--- App7_Webscraping/realEstate.py
+++ App7_Webscraping/realEstate.py
@@ -11,5 +11,25 @@ content = r.content
 soup = BeautifulSoup(content, "html.parser")
 
 property_rows = soup.find_all("div",{"class": "propertyRow"})
-prices = property_rows[0].find("h4", {"class": "propPrice"}).text.replace("\n", "").replace(" ", "")
-print(prices)
+
+for row in property_rows:
+    price = row.find("h4", {"class": "propPrice"}).text.strip()
+    address = row.find_all("span", {"class": "propAddressCollapse"})
+    try:
+        beds = row.find("span", {"class": "infoBed"}).find("b").text
+    except AttributeError:
+        beds = "N/A"
+    try:
+        area = row.find("span", {"class": "infoSqFt"}).find("b").text
+    except AttributeError:
+        area = "N/A"
+    try:
+        full_baths = row.find("span", {"class": "infoValueFullBath"}).find("b").text
+    except AttributeError:
+        full_baths = "N/A"
+    try:
+        half_baths = row.find("span", {"class": "infoValueHalfBath"}).find("b").text
+    except AttributeError:
+        half_baths = "N/A"
+
+    print(price, address[0].text, address[1].text, beds, area, full_baths, half_baths, "\n")
