3ca97d1b71bc01a35918706aafaad89fc7f61035
isulim <ihor.sulym@gmail.com>
2019-11-04 22:23:25 +0100

    Embedded Bokeh plot to Flask website Deployed to Heroku

--- App8_FinancialGraph/chart.py
+++ App8_FinancialGraph/chart.py
@@ -1,36 +1,44 @@
 from pandas_datareader import data
 from datetime import datetime, timedelta
 from bokeh.plotting import figure, show, output_file
+from bokeh.embed import components
+from bokeh.resources import CDN
 
-start = datetime(2019, 3, 1)
-end = datetime(2019, 10, 10)
-apple = data.DataReader(name="AAPL", data_source="yahoo", start=start, end=end)
 
-p = figure(x_axis_type='datetime', width=1000, height=400, sizing_mode="scale_width")
-p.title.text = "Candlestick chart"
-p.grid.grid_line_alpha = 0.5
+def generate_plot(start=(datetime.today()-timedelta(days=60)), end=datetime.today(), company="AAPL"):
 
-hossa = apple.index[apple.Close > apple.Open]
-bessa = apple.index[apple.Close < apple.Open]
-stable = apple.index[apple.Close == apple.Open]
+    start = start
+    end = end
+    company = data.DataReader(name=company, data_source="yahoo", start=start, end=end)
 
-for day in hossa:
-    p.segment(x0=day, y0=apple.loc[day].High, x1=day, y1=apple.loc[day].Low, color='black')
-    p.quad(left=day - timedelta(hours=6), right=day + timedelta(hours=6),
-           top=apple.loc[day].Close, bottom=apple.loc[day].Open, fill_color='green',
-           line_color='black')
+    p = figure(x_axis_type='datetime', width=1000, height=400, sizing_mode="scale_width")
+    p.title.text = "Candlestick chart"
+    p.grid.grid_line_alpha = 0.5
 
-for day in bessa:
-    p.segment(x0=day, y0=apple.loc[day].High, x1=day, y1=apple.loc[day].Low, color='black')
-    p.quad(left=day - timedelta(hours=6), right=day + timedelta(hours=6),
-           top=apple.loc[day].Open, bottom=apple.loc[day].Close, fill_color='red',
-           line_color='black')
+    hossa = company.index[company.Close > company.Open]
+    bessa = company.index[company.Close < company.Open]
+    stable = company.index[company.Close == company.Open]
 
-for day in stable:
-    p.segment(x0=day, y0=apple.loc[day].High, x1=day, y1=apple.loc[day].Low, color='black')
-    p.quad(left=day - timedelta(hours=6), right=day + timedelta(hours=6),
-           top=apple.loc[day].Open, bottom=apple.loc[day].Close, fill_color='grey',
-           line_color='black')
+    for day in hossa:
+        p.segment(x0=day, y0=company.loc[day].High, x1=day, y1=company.loc[day].Low, color='black')
+        p.quad(left=day - timedelta(hours=6), right=day + timedelta(hours=6),
+               top=company.loc[day].Close, bottom=company.loc[day].Open, fill_color='green',
+               line_color='black')
 
-output_file('CS.html')
-show(p)
+    for day in bessa:
+        p.segment(x0=day, y0=company.loc[day].High, x1=day, y1=company.loc[day].Low, color='black')
+        p.quad(left=day - timedelta(hours=6), right=day + timedelta(hours=6),
+               top=company.loc[day].Open, bottom=company.loc[day].Close, fill_color='red',
+               line_color='black')
+
+    for day in stable:
+        p.segment(x0=day, y0=company.loc[day].High, x1=day, y1=company.loc[day].Low, color='black')
+        p.quad(left=day - timedelta(hours=6), right=day + timedelta(hours=6),
+               top=company.loc[day].Open, bottom=company.loc[day].Close, fill_color='grey',
+               line_color='black')
+
+    js_script, div = components(p)
+    cdn_js = CDN.js_files
+    cdn_css = CDN.css_files
+
+    return js_script, div, cdn_js, cdn_css
--- README.md
+++ README.md
@@ -21,8 +21,9 @@ Script blocks sites by redirecting their URLs to localhost by editing 'hosts' fi
 
 ## App4 - Flask Website
 
-Smallest possible website made in Flask, deployed on Heroku.<br>
-[Heroku deploy](https://morning-harbor-41773.herokuapp.com/)
+Smallest website made in Flask, deployed on Heroku.<br>
+[Heroku deploy](https://flaskbokeh12345.herokuapp.com/)<br>
+Later embedded a plot from App8.
 
 ## App5 - Bookshelf
 
@@ -32,3 +33,12 @@ User can add, view, update, delete and search books.
 ## App6 - Motion Detection
 App to detect objects moving in sight of webcam (OpenCV).<br>
 App records times when any object enters webcam's field of view and when all objects are gone.
+
+## App7 - Webscrapping
+App to scrap website of real estate data.
+Output scrapped info to a CSV file.
+
+## App8 - Financial graph
+Financial graph generated with bokeh.<br>
+Plot is embedded in a website from App4 deployed online.<br>
+[Heroku deploy](https://flaskbokeh12345.herokuapp.com/)
