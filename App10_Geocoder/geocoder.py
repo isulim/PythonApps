cbc5bc30d370040a17615cf75cebbd439a3dadd1
isulim <ihor.sulym@gmail.com>
2019-11-27 22:55:01 +0100

    Handle KeyError when no 'Address' or 'address' column is present

--- App10_Geocoder/geocoder.py
+++ App10_Geocoder/geocoder.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from pandas import read_csv
+from geopy.geocoders import ArcGIS
 
 app = Flask(__name__)
 
@@ -17,6 +18,18 @@ def geocode():
         file.save(secure_filename("uploaded"+file.filename))
         with open("uploaded"+file.filename) as f:
             data = read_csv(f)
+            try:
+                addresses = data.loc[:, 'Address']
+            except KeyError:
+                try:
+                    addresses = data.loc[:, 'address']
+                except KeyError:
+                    return render_template("index.html", text="""<h2>ERROR</h2>
+                                                                 <h4>No column named 
+                                                                 <i>Address</i> or <i>address</i> 
+                                                                 found in your file.<br>
+                                                                 Please try again.</h4>""")
+
         return render_template("geocode.html", table=data.to_html())
     return render_template("index.html", text="Something went wrong."
                                               "<br>Check your file and "
